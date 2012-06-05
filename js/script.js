d4fd9fb9203c99b17df0a8ea4207577af7a984cc
Peter Magenheimer <peter.magenheimer@gmail.com>
2012-06-04 18:48:46 -0600

    Randomize spin degrees with JS.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,26 @@
 
 */
 
+$(document).ready(function() {
+    $('head').append($('<style id="keyframes"/>'))
+    $('#spin').click(function() {
+        $('.reel').removeClass('active');
+        var end_degrees = (Math.floor(Math.random()*12) * 30) + 3600;
+        var bounce = Math.floor(Math.random()*14) + end_degrees;
+        $('#keyframes').text("@-webkit-keyframes spin {\
+            0% {\
+                -webkit-transform: rotateX(0);\
+            }\
+            97% {\
+                -webkit-transform: rotateX(-" + bounce + "deg);\
+            }\
+            100% {\
+                -webkit-transform: rotateX(-" + end_degrees + "deg);\
+            }\
+        }");
+        $('.reel').addClass('active');
+    });
+});
 
 
 
