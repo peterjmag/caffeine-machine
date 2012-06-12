7f397020fa516208af3f9aa9efb9fbd9cece3e07
Peter Magenheimer <peter.magenheimer@gmail.com>
2012-06-12 16:57:42 -0600

    Move spin code into named function.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,62 @@
 
 */
 
+function spin_reels() {
+    $('.reel').removeClass('active');
+
+    var slot_types = ['coffee', 'tea', 'espresso'];
+    var reel_params = [];
+    while(reel_params.length < 3) {
+        var reel = [];
+        reel['end_slot'] = Math.floor(Math.random()*12) + 1;
+        reel['end_slot_type'] = slot_types[(reel['end_slot'] % 3)];
+        reel['end_degrees'] = (reel['end_slot'] * 30) + 3600;
+        reel['bounce'] = Math.floor(Math.random()*14) + reel['end_degrees'];
+        reel_params.push(reel);
+    }
+
+    $('#keyframes').text("\
+        @-webkit-keyframes reel-1-spin {\
+            0% {\
+                -webkit-transform: rotateX(0);\
+            }\
+            97% {\
+                -webkit-transform: rotateX(-" + reel_params[0]['bounce'] + "deg);\
+            }\
+            100% {\
+                -webkit-transform: rotateX(-" + reel_params[0]['end_degrees'] + "deg);\
+            }\
+        }\
+        @-webkit-keyframes reel-2-spin {\
+            0% {\
+                -webkit-transform: rotateX(0);\
+            }\
+            97% {\
+                -webkit-transform: rotateX(-" + reel_params[1]['bounce'] + "deg);\
+            }\
+            100% {\
+                -webkit-transform: rotateX(-" + reel_params[1]['end_degrees'] + "deg);\
+            }\
+        }\
+        @-webkit-keyframes reel-3-spin {\
+            0% {\
+                -webkit-transform: rotateX(0);\
+            }\
+            97% {\
+                -webkit-transform: rotateX(-" + reel_params[2]['bounce'] + "deg);\
+            }\
+            100% {\
+                -webkit-transform: rotateX(-" + reel_params[2]['end_degrees'] + "deg);\
+            }\
+        }\
+        ");
+
+    $('.reel').addClass('active');
+
+    $("#reel-3").unbind('animationend webkitAnimationEnd')
+        .bind('animationend webkitAnimationEnd', {reel_params: reel_params}, get_results);
+}
+
 function get_results(event) {
     var r1 = event.data.reel_params[0]['end_slot_type'];
     var r2 = event.data.reel_params[1]['end_slot_type'];
@@ -14,61 +70,7 @@ function get_results(event) {
 $(document).ready(function() {
     $('head').append($('<style id="keyframes"/>'));
 
-    $('#spin').click(function() {
-        $('.reel').removeClass('active');
-
-        var slot_types = ['coffee', 'tea', 'espresso'];
-        var reel_params = [];
-        while(reel_params.length < 3) {
-            var reel = [];
-            reel['end_slot'] = Math.floor(Math.random()*12) + 1;
-            reel['end_slot_type'] = slot_types[(reel['end_slot'] % 3)];
-            reel['end_degrees'] = (reel['end_slot'] * 30) + 3600;
-            reel['bounce'] = Math.floor(Math.random()*14) + reel['end_degrees'];
-            reel_params.push(reel);
-        }
-
-        $('#keyframes').text("\
-            @-webkit-keyframes reel-1-spin {\
-                0% {\
-                    -webkit-transform: rotateX(0);\
-                }\
-                97% {\
-                    -webkit-transform: rotateX(-" + reel_params[0]['bounce'] + "deg);\
-                }\
-                100% {\
-                    -webkit-transform: rotateX(-" + reel_params[0]['end_degrees'] + "deg);\
-                }\
-            }\
-            @-webkit-keyframes reel-2-spin {\
-                0% {\
-                    -webkit-transform: rotateX(0);\
-                }\
-                97% {\
-                    -webkit-transform: rotateX(-" + reel_params[1]['bounce'] + "deg);\
-                }\
-                100% {\
-                    -webkit-transform: rotateX(-" + reel_params[1]['end_degrees'] + "deg);\
-                }\
-            }\
-            @-webkit-keyframes reel-3-spin {\
-                0% {\
-                    -webkit-transform: rotateX(0);\
-                }\
-                97% {\
-                    -webkit-transform: rotateX(-" + reel_params[2]['bounce'] + "deg);\
-                }\
-                100% {\
-                    -webkit-transform: rotateX(-" + reel_params[2]['end_degrees'] + "deg);\
-                }\
-            }\
-            ");
-
-        $('.reel').addClass('active');
-
-        $("#reel-3").unbind('animationend webkitAnimationEnd')
-            .bind('animationend webkitAnimationEnd', {reel_params: reel_params}, get_results);
-    });
+    $('#spin').click(spin_reels);
 });
 
 
